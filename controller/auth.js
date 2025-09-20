635899e4e3fb94a1245eaf9cfeb7c06f545618e5
samiullah239 <2014tabontech@gmail.com>
2025-09-20 01:04:35 -0700

    done

--- controller/auth.js
+++ controller/auth.js
@@ -67,13 +67,13 @@ export const signIn = async (req, res) => {
   }
 };
 
-// const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-// const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-// const REDIRECT_URI = process.env.GOOGLE_REDIRECT_URI;
+const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+const REDIRECT_URI = process.env.GOOGLE_REDIRECT_URI;
 
-const CLIENT_ID = "1072288734636-og1s7nku04nb0gf56v53gr8uar1tjjpq.apps.googleusercontent.com";
-const CLIENT_SECRET = "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";
-const REDIRECT_URI = "http://localhost:5000/auth/google/callback";
+// const CLIENT_ID = "1072288734636-og1s7nku04nb0gf56v53gr8uar1tjjpq.apps.googleusercontent.com";
+// const CLIENT_SECRET = "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";
+// const REDIRECT_URI = "http://localhost:5000/auth/google/callback";
 console.log('CLIENT_ID', CLIENT_ID);
 console.log('CLIENT_SECRET', CLIENT_SECRET);
 console.log('REDIRECT_URI', REDIRECT_URI);
