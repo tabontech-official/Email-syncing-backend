5daecddf42ead9b66eeb8fea463f33532612b5fe
samiullah239 <2014tabontech@gmail.com>
2025-09-29 05:55:37 -0700

    done

--- controller/delayWorker.js
+++ controller/delayWorker.js
@@ -66,8 +66,8 @@ import cron from "node-cron";
 import mongoose from "mongoose";
 import { sendEmailModule } from "./smtpServer.js";
 import { DelayJobModel } from "../Models/DelayJob.js";
-import { TemplateModel } from "../Models/TemplateModel.js";
 import { scenarioModel } from "../Models/scenarioModel.js";
+import { TemplateModel } from "../Models/Template.js";
 
 export const startDelayWorker = () => {
   cron.schedule("* * * * *", async () => {
