057fb3d3b43000b62263a40ccd9f18f683fe54a2
samiullah239 <2014tabontech@gmail.com>
2025-10-22 05:18:31 -0700

    sone

--- Models/Scenario.js
+++ Models/Scenario.js
@@ -29,7 +29,7 @@ const ModuleSchema = new mongoose.Schema({
   delayValue: { type: Number, default: null },
   delayUnit: { type: String, enum: ["seconds", "minutes", "hours"], default: null },
 
-  emailType: { type: String, enum: ["Gmail", "Email", ""], default: "" },
+  emailType: { type: String, enum: ["Gmail", "Email", "Delay"], default: "" },
 
   filter: {
     label: String,
