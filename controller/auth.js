1b91afd2f2bbc521c624f4357859e52c6c7402fb
samiullah239 <2014tabontech@gmail.com>
2025-10-28 04:45:13 -0700

    sone

--- controller/auth.js
+++ controller/auth.js
@@ -711,11 +711,14 @@ export const skipAllSteps = async (req, res) => {
   }
 };
 
+// const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+// const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+// const REDIRECT_URI = process.env.GOOGLE_REDIRECT_URI;
 
 const CLIENT_ID =
   '1072288734636-og1s7nku04nb0gf56v53gr8uar1tjjpq.apps.googleusercontent.com';
 const CLIENT_SECRET = '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';
-const REDIRECT_URI = 'https://email-syncing-backend.vercel.app/auth/google/callback';
+const REDIRECT_URI = 'http://localhost:5000/auth/google/callback';
 console.log('CLIENT_ID', CLIENT_ID);
 console.log('CLIENT_SECRET', CLIENT_SECRET);
 console.log('REDIRECT_URI', REDIRECT_URI);
@@ -772,7 +775,7 @@ export const googleAuthCallback = async (req, res) => {
 
     if (!tokens.refresh_token) {
       return res.redirect(
-        'https://zenith-inbox-frontend.vercel.app/connection?status=no_refresh_token'
+        'http://localhost:3006/connection?status=no_refresh_token'
       );
     }
 
@@ -812,11 +815,11 @@ export const googleAuthCallback = async (req, res) => {
     await connection.save();
 
     return res.redirect(
-      `https://zenith-inbox-frontend.vercel.app/scenarios/shopify?google-auth-success=true&connectionId=${connection._id}`
+      `http://localhost:3006/scenarios/shopify?google-auth-success=true&connectionId=${connection._id}`
     );
   } catch (error) {
     console.error('❌ Error during Google auth callback:', error);
-    return res.redirect('https://zenith-inbox-frontend.vercel.app/connection?status=error');
+    return res.redirect('http://localhost:3006/connection?status=error');
   }
 };
 
@@ -905,8 +908,8 @@ export const addSmtpConnection = async (req, res) => {
 
 const MICROSOFT_CLIENT_ID = '09979dca-57cd-450e-8934-24887f1f368c';
 const MICROSOFT_CLIENT_SECRET = 'TQK8Q~Awgm.47QKh2QT5w~D4nqZiwkGGJpoQ5c._';
-const MICROSOFT_REDIRECT_URI = 'https://email-syncing-backend.vercel.app/auth/outlook/callback';
-const FRONTEND_URL = 'https://zenith-inbox-frontend.vercel.app';
+const MICROSOFT_REDIRECT_URI = 'http://localhost:5000/auth/outlook/callback';
+const FRONTEND_URL = 'http://localhost:3006';
 
 const oauthConfig = {
   client: {
@@ -998,7 +1001,7 @@ export const outlookOAuthCallback = async (req, res) => {
     await connection.save();
 
     return res.redirect(
-      `https://zenith-inbox-frontend.vercel.app/scenarios/shopify?google-auth-success=true&connectionId=${connection._id}`
+      `http://localhost:3006/scenarios/shopify?google-auth-success=true&connectionId=${connection._id}`
     );
   } catch (err) {
     console.error(' Outlook OAuth error:', err);
@@ -1022,7 +1025,7 @@ export const forgotPassword = async (req, res) => {
 
     const token = createToken({ id: user._id });
 
-    const resetUrl = `https://zenith-inbox-frontend.vercel.app/reset-password/${token}`;
+    const resetUrl = `http://localhost:3006/reset-password/${token}`;
 
     const transporter = nodemailer.createTransport({
       service: 'gmail',
