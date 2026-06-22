d7b88fee72c9c093ca95b8073cb4a27830b029c3
Samiullahqureshi1 <samiullahqureshi669@gmail.com>
2026-06-22 10:47:45 +0500

    first commit

--- controller/auth.js
+++ controller/auth.js
@@ -1117,10 +1117,14 @@ export const skipAllSteps = async (req, res) => {
   }
 };
 
+// const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+// const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+// const REDIRECT_URI = process.env.GOOGLE_REDIRECT_URI;
+
+
 const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 const REDIRECT_URI = process.env.GOOGLE_REDIRECT_URI;
-
 
 
 const oauth2Client = new google.auth.OAuth2(
