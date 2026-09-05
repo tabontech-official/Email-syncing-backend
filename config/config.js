2e51428db9e55eb3303fe8dd80f7d735db13b2a9
Malik Rehan <68987335+malikmuhammadrehan@users.noreply.github.com>
2026-09-05 23:04:39 +0500

    newone

--- config/config.js
+++ config/config.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 // Load environment variables from .env file into process.env
 dotenv.config(); 
 
+
+
 // Export the environment variables as a config object
 const config = {
   port: process.env.PORT || 5000,
